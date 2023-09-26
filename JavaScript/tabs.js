4914117988a61c3bf33ee9635715f4ea0f33e48d
96DiogoBraga <96diogobraga@gmail.com>
2023-09-25 23:48:27 -0300

    tabs e parallax

---
+++ JavaScript/tabs.js
@@ -0,0 +1,9 @@
+function showConsultoria() {
+    document.getElementById("tab-consultoria").style.display='flex'
+    document.getElementById("tab-servicos").style.display='none'
+}
+
+function showServicos() {
+    document.getElementById("tab-consultoria").style.display='none'
+    document.getElementById("tab-servicos").style.display='flex'
+}
